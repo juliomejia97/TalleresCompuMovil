faabb3414d7294289f11ebade650c93eae97e0fe
Julio Mejía <julio.mejia@javeriana.edu.co>
2020-09-02 15:26:01 -0500

    Fibonnaci Excercise done!

---
+++ Taller 1/app/src/main/java/com/example/taller1/FibonnaciActivity.java
@@ -0,0 +1,57 @@
+package com.example.taller1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.mtp.MtpConstants;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.ListView;
+
+import java.lang.reflect.Array;
+import java.util.List;
+
+public class FibonnaciActivity extends AppCompatActivity {
+    private String[] sequence;
+    private ListView listSequence;
+    private ImageButton creador;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fibonnaci);
+        Intent intent = getIntent();
+        //Llenar la secuencia fibonacci
+        Integer number = intent.getIntExtra("number",0);
+        generarSecuencia(number);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, sequence);
+        listSequence = findViewById(R.id.lista);
+        listSequence.setAdapter(adapter);
+        //Inflar el boton de imagen
+        creador = findViewById(R.id.creador);
+        creador.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri webpage = Uri.parse("https://es.wikipedia.org/wiki/Sucesi%C3%B3n_de_Fibonacci");
+                Intent wikiFibonacci = new Intent(Intent.ACTION_VIEW,webpage);
+                startActivity(wikiFibonacci);
+            }
+        });
+    }
+
+    private void generarSecuencia(int n){
+        sequence = new String[n];
+        for(int i=0; i < n; i++){
+            sequence[i] = getNumberFibo(i);
+        }
+    }
+
+    private String getNumberFibo(int n){
+        double A = (1 + Math.sqrt(5))/2;
+        double B = (1 - Math.sqrt(5))/2;
+        double fib = (Math.pow(A,n) - Math.pow(B,n))/ Math.sqrt(5);
+        return String.valueOf((int) Math.floor(fib));
+    }
+}
--- Taller 1/app/src/main/java/com/example/taller1/MainActivity.java
+++ Taller 1/app/src/main/java/com/example/taller1/MainActivity.java
@@ -2,13 +2,57 @@ package com.example.taller1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ArrayList<String> options;
+    private Spinner spinnerOptions;
+    private ArrayAdapter<String> adapter;
+    private Button fiboButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Inicializar els spinner
+        initOptions();
+        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,options);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerOptions = findViewById(R.id.options);
+        spinnerOptions.setAdapter(adapter);
+        //Inflar boton de Fibo
+        fiboButton = findViewById(R.id.fibo);
+        fiboButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText number = findViewById(R.id.posiciones);
+                //Si está lleno el campo hacer intent, sino mostar toast
+                if(number.getText().length()==0) {
+                    Toast.makeText(view.getContext(), "¡No se ha ingresado un número!", Toast.LENGTH_SHORT).show();
+                }else{
+                    Integer posiciones = Integer.parseInt(String.valueOf(number.getText()));
+                    Intent intent = new Intent(view.getContext(),FibonnaciActivity.class);
+                    intent.putExtra("number",posiciones);
+                    startActivity(intent);
+                }
+            }
+        });
+
+    }
+
+    void initOptions(){
+        options = new ArrayList<String>();
+        for(int i=0; i <= 15; i++)
+            options.add(String.valueOf(i));
     }
 }
